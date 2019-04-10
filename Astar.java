37166b91ea712639cd060e989fe02ea9d41ec855
Ayça Yiğit <aychayigit1998@gmail.com>
2019-04-11 00:32:15 +0300

    Add files via upload

---
+++ Astar.java
@@ -0,0 +1,21 @@
+
+package astar;
+
+/**
+ *
+ * @author aycha
+ */
+public class Astar {
+
+   
+    public static void main(String[] args) throws Exception {
+        
+        String s=new String();
+        Astars a1=new Astars();
+        a1.getState();
+        a1.display();
+        s=a1.TreeSearch();
+        System.out.println(s);
+    }
+    
+}
